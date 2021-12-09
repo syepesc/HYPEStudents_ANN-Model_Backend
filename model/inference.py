8062b6d55f4ad94349e075d01bad80f9945bf6f9
Santiago Yepes Carrera <syepesc@Santiagos-MacBook-Pro.local>
2021-12-09 14:10:19 -0500

    Update model 09/12/2021

--- model/inference.py
+++ model/inference.py
@@ -1,6 +1,10 @@
 import pandas as pd, pickle, tensorflow as tf
 
-sample = [{'INTAKE TERM CODE': 2021, 'ADMIT TERM CODE': 2021, 'INTAKE COLLEGE EXPERIENCE': 'Enrolled', 'PRIMARY PROGRAM CODE': 8226, 'SCHOOL CODE': 'TR', 'STUDENT LEVEL NAME': 'Post Secondary', 'TIME STATUS NAME': 'Full-Time', 'FUNDING SOURCE NAME': 'GPOG - FT', 'MAILING POSTAL CODE GROUP 3': 'M4H', 'GENDER': 'M', 'DISABILITY IND': 'N', 'FUTURE TERM ENROL': '1-0-0-0-0-0-0-0-0-0', 'ACADEMIC PERFORMANCE': 'C - Satisfactory', 'EXPECTED GRAD TERM CODE': 2023, 'FIRST YEAR PERSISTENCE COUNT': 0, 'HS AVERAGE MARKS': 75, 'ENGLISH TEST SCORE': 130, 'AGE GROUP LONG NAME': '0 to 18', 'FIRST GENERATION IND': 'N', 'effective academic history': 'high school', 'APPLICANT TARGET SEGMENT NAME': 'Direct Entry'}]
+sample = [{'INTAKE TERM CODE': 2020, 'ADMIT TERM CODE': 2020, 'INTAKE COLLEGE EXPERIENCE': 'New to College', 'PRIMARY PROGRAM CODE': 9111, 'SCHOOL CODE': 'CH',
+                'STUDENT LEVEL NAME': 'Post Secondary', 'MAILING POSTAL CODE GROUP 3': 'L1V', 'GENDER': 'F', 'DISABILITY IND': 'N', 'FUTURE TERM ENROL': '1-1-1-1-0-0-0-0-0-0',
+                'ACADEMIC PERFORMANCE': 'DF - Poor', 'EXPECTED GRAD TERM CODE': 2020, 'FIRST YEAR PERSISTENCE COUNT': 0, 'HS AVERAGE MARKS': 0, 'ENGLISH TEST SCORE': 140,
+				'AGE GROUP LONG NAME': '41 to 50', 'FIRST GENERATION IND': 'Y', 'effective academic history': 'no', 'APPLICANT TARGET SEGMENT NAME': 'Non-Direct Entry',
+				'TIME STATUS NAME': 'Full-Time', 'FUNDING SOURCE NAME': 'GPOG - FT'}]
 sample = pd.DataFrame(sample)
 file = open('encoder', 'rb')
 deserializer = pickle.load(file)
@@ -9,12 +13,12 @@ pca = deserializer['pca']
 file.close()
 
 # encoding categorical features
-categorical_column = [col for col in sample.columns if sample[col].dtype == 'object']
+categorical_column = [col for col in sample.columns if sample[col].dtype == 'object'] + ['PRIMARY PROGRAM CODE', 'INTAKE TERM CODE', 'ADMIT TERM CODE', 'EXPECTED GRAD TERM CODE']
 for column in categorical_column:
 	encoder = label_encoders[column]
 	print(column + ':')
 	print(encoder.classes_)
-	sample[column] = pd.Series(encoder.fit_transform(sample[column][sample[column].notna()]), index=sample[column][sample[column].notna()].index)
+	sample[column] = pd.Series(encoder.transform(sample[column][sample[column].notna()]), index=sample[column][sample[column].notna()].index)
 # feature reduction
 reduced_feature = pca.transform(sample[['FUNDING SOURCE NAME', 'TIME STATUS NAME']])  # FUNDING SOURCE NAME is highly correlated with TIME STATUS NAME
 sample.insert(6, 'time and fund', reduced_feature)
